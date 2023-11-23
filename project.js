9e555bce3a9c08f486b91329195ce25a9941a0ab
Antkilla <132300802+Antkilla@users.noreply.github.com>
2023-11-22 20:06:57 -0800

    Update project.js

--- project.js
+++ project.js
@@ -10,6 +10,22 @@
 
 const  prompt = require("prompt-sync")();
 
+const Rows = 3
+const Cols = 3
+
+const SymbolsCount = {
+    'A': 2,
+    'B': 4,
+    'C': 6,
+    'D': 8
+}
+
+const SymbolsValue = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
 
 const deposit = () => {
     while (true) {
@@ -25,4 +41,35 @@ const deposit = () => {
     }   
 };
 
-deposit();
+const getNumberOfLines = () =>  {
+    while (true) {
+        const lines= prompt("Enter the number of lines to bet on (1-3): ")
+        const numberOfLines = parseFloat(lines);
+
+        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+            console.log("Invalid number of lines, try again.");
+        }   else {
+            return numberOfLines;
+        } 
+    }    
+};    
+
+const getBet = (balance, lines) => {
+    while (true) {
+        const bet= prompt("Enter the bet per line: ")
+        const numberBet = parseFloat(bet);
+
+        if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+            console.log("Invalid bet, try again.");
+        }   else {
+            return numberBet;
+        } 
+    }   
+
+}
+
+
+
+let balance = deposit();
+const numberOfLines = getNumberOfLines();
+const bet = getBet(balance, numberOfLines);
